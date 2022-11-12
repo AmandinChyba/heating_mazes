28ce7cab897414b22299111e6bdc63ced6ed6d28
AmandinChyba <achybarabeendran@mines.edu>
2022-11-11 23:38:24 -0700

    saving plotoptix data in file

--- heat_solver.py
+++ heat_solver.py
@@ -99,9 +99,9 @@ def SolveHeat(u):
     return u, E, max_time, inter_coord
 
 def plotoptix_data(u):
-    u_k = u.copy()
+    u = u.copy()
 
-    B = np.zeros(u_k.shape)
+    B = np.zeros(u.shape)
     '''
     rise_time = 10
     for t in range(T-1):
@@ -115,11 +115,11 @@ def plotoptix_data(u):
     height_change = 0.02
     for t in range(T-1):
         for i,j in idx:
-            u0 = u_k[t][i][j]
-            u1 = u_k[t][i+1][j]
-            u2 = u_k[t][i-1][j]
-            u3 = u_k[t][i][j+1]
-            u4 = u_k[t][i][j-1]
+            u0 = u[t][i][j]
+            u1 = u[t][i+1][j]
+            u2 = u[t][i-1][j]
+            u3 = u[t][i][j+1]
+            u4 = u[t][i][j-1]
             if u1 == -1.0: u1 = u0
             if u2 == -1.0: u2 = u0
             if u3 == -1.0: u3 = u0
@@ -138,9 +138,17 @@ def plotoptix_data(u):
             else:
                 if B[t,i,j] > 0:
                     B[t+1,i,j] = B[t,i,j] - height_change
-   
+
+    # normalize the values so that the max = 1
+    for t in range(T-1):
+        for i,j in idx:
+            u[t,i,j] = u[t,i,j] * 1 / np.max(u[t])
+        u[t,start[0],start[1]] = 1.2
+        u[t,end[0],end[1]] = 1.2
+
     # save B and u in .npy file
-    #np.save([u, B], 'maze_data_plotoptix.npy') 
+    optix_data = np.array([maze, u, B], dtype="object")
+    np.save('maze_data_plotoptix.npy', optix_data) 
     
     return B
 
@@ -160,7 +168,7 @@ PlotShortestPath2(u[max_time-1], maze, start, end)
 #PlotShortestPath2(u[max_time-1], maze, inter_coord, end)
 
 # create animation
-AnimateSolution(u[:max_time-1], start, end, max_time-1, frame_skip=20, 'solution.gif')
-AnimateSolution(B, start, end, T-1, frame_skip=20, file_name='heat_change.gif')
+#AnimateSolution(u[:max_time-1], start, end, max_time-1, frame_skip=20, file_name='solution.gif')
+AnimateSolution(B, start, end, T-1, frame_skip=20, file_name='change_heat.gif')
 
 
